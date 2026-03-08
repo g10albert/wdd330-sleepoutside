38db33dc5987be24a7d8b1d3d48cf210068d48be
Crispus-256 <nuweamanyacrispusinfinityeneri@gmail.com>
2026-03-08 15:37:01 +0300

    fixed a bug in product.js

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,17 @@
-import { setLocalStorage } from "./utils.mjs";
+// We bring in 'helpers' from other files.
+// utils.mjs' handles the browser's storage,
+// and 'ProductData.mjs' handles fetching our tent data.
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  const cartItems = getLocalStorage("so-cart") || []; // get cart array of items from local storage if null set to empty array
+  cartItems.push(product);
+  setLocalStorage("so-cart", cartItems);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
@@ -13,6 +19,8 @@ async function addToCartHandler(e) {
 }
 
 // add listener to Add to Cart button
+// We tell the browser: "Find the button with ID 'addToCart'.
+// Wait for a 'click', and when it happens, run the addToCartHandler."
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);
